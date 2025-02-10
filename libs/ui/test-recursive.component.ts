b2f1ab14b2cf2ec78a397b90950c1159593fb26c
SkipThorp-Colibri <84403273+SkipThorp-Colibri@users.noreply.github.com>
2025-02-10 16:19:19 -0600

    add TestRecursiveComponent with HTML, SCSS, and routing integration

---
+++ libs/ui/test-recursive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'recursive-hierarchy-list',
+  imports: [CommonModule],
+  templateUrl: './test-recursive.component.html',
+  styleUrl: './test-recursive.component.scss'
+})
+export class TestRecursiveComponent {}
