a0c2f3232a5e3ce9b911cea3813e8842ac032b5a
SkipThorp-Colibri <84403273+SkipThorp-Colibri@users.noreply.github.com>
2025-02-14 12:48:08 -0600

    refactor: add recursive collapse functionality for child groups in Group model

--- src/models/group.model.ts
+++ src/models/group.model.ts
@@ -19,6 +19,23 @@ class Group implements IGroup, IGroupMethods {
 
   toggleGroup(): void {
     this.expanded = !this.expanded;
+
+    // Recursively collapse all child groups if this group is collapsed
+    // If collapsing, set all child groups' 'expanded' to false
+    if (!this.expanded && this.groups.length > 0) {
+      this.setAllChildGroupsCollapsed(this);
+    }
+  }
+
+  // Helper method to set all child groups to collapsed
+  private setAllChildGroupsCollapsed(group: Group): void {
+    group.groups.forEach(childGroup => {
+      childGroup.expanded = false;
+      // If the child group has its own children, collapse them as well
+      if (childGroup.groups.length > 0) {
+        this.setAllChildGroupsCollapsed(childGroup);
+      }
+    });
   }
 
   isGroup(): boolean {
