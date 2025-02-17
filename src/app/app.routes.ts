1b914502536341424311339b9e4eb53c7c6f7e12
SkipThorp-Colibri <84403273+SkipThorp-Colibri@users.noreply.github.com>
2025-02-17 07:47:25 -0600

    refactor: update header styling for Recursive Group Examples and remove unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { RecursiveGroupListComponent } from '../../libs/ui/basic/recursive-group-list.component';
 
-export const routes: Routes = [
-  { path: 'basic', component: RecursiveGroupListComponent },
-  { path: '', redirectTo: '/basic', pathMatch: 'full' }, // Default route (optional)
-  { path: '**', redirectTo: '/basic' } // Wildcard route (optional)
-];
+export const routes: Routes = [];
