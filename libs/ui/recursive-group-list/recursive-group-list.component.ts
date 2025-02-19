34036b974acaa8ba59082c6d491a504e0aee7e3d
SkipThorp-Colibri <84403273+SkipThorp-Colibri@users.noreply.github.com>
2025-02-19 09:22:51 -0600

    refactor: clean up import statement for Group model in RecursiveGroupListComponent

--- libs/ui/recursive-group-list/recursive-group-list.component.ts
+++ libs/ui/recursive-group-list/recursive-group-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Group } from '../../../src/models/group.model';  // Import the Group class
+import { Group } from '../../../src/models/group.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
