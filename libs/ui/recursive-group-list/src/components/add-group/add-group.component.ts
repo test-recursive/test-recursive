1a73e2a6b5188e6c2fb9f92fa5ced7e8db098c07
SkipThorp-Colibri <84403273+SkipThorp-Colibri@users.noreply.github.com>
2025-02-26 11:28:31 -0600

    feat: add AddGroupComponent with template and initial structure

---
+++ libs/ui/recursive-group-list/src/components/add-group/add-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'clx-add-group',
+  imports: [CommonModule],
+  templateUrl: './add-group.component.html'
+})
+export class AddGroupComponent {
+
+
+}
