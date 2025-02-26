8943da0df6f635a7ce9529cd03c62ccefd1f4dff
SkipThorp-Colibri <84403273+SkipThorp-Colibri@users.noreply.github.com>
2025-02-26 15:49:30 -0600

    feat: add MoveGroupComponent with initial structure and template

---
+++ libs/ui/recursive-group-list/src/components/move-group/move-group.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'clx-move-group',
+  imports: [CommonModule],
+  templateUrl: './move-group.component.html'
+})
+export class MoveGroupComponent {}
